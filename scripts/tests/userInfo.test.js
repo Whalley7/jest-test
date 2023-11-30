68a28461d4cd64be409525f1ad80c3e28228591a
Whalley7 <Hallettwendy@GMAIL.com>
2023-11-30 00:06:34 +0000

    username-score-level-test

---
+++ scripts/tests/userInfo.test.js
@@ -0,0 +1,16 @@
+// userInfo.test.js
+const getUserInfo = require('../userInfo');
+
+test('getUserInfo returns the correct user information', () => {
+  const user = getUserInfo();
+
+  // Check if the properties exist
+  expect(user).toHaveProperty('username');
+  expect(user).toHaveProperty('level');
+  expect(user).toHaveProperty('score');
+
+  // Check the values of the properties
+  expect(user.username).toBe('JohnDoe');
+  expect(user.level).toBe(5);
+  expect(user.score).toBe(1000);
+});
